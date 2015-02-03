ecfcd235f4e1871aa33d2054eac0f6359abd3e64
sle-guil <sle-guil@student.42.fr>
2015-02-03 16:12:27 +0100

    Output function work

--- suliac.c
+++ suliac.c
@@ -6,12 +6,14 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/03 11:35:38 by sle-guil          #+#    #+#             */
-/*   Updated: 2015/02/03 15:11:51 by sle-guil         ###   ########.fr       */
+/*   Updated: 2015/02/03 16:12:10 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
+#include <stdio.h>
 #include <time.h>
+#include "puiss4.h"
 
 void	clean_ret(int *ret)
 {
@@ -22,9 +24,12 @@ void	clean_ret(int *ret)
 		if (ret[i] < 0)
 		{
 			pow = 1;
-			while (ret[i] / pow > 0)
+			while (!(ret[i] % 2))
+			{
+				ret[i] /= 10;
 				pow *= 10;
-			ret [i] = pow;
+			}
+			ret [i] = (-1) * pow;
 		}
 }
 
@@ -43,20 +48,24 @@ int		get_bestindice(int best, int i)
 
 int		drop_piece(int *ret)
 {
-	int		defeat = 0;
+	int		best = 0;
 	int		i;
 
 	clean_ret(ret);
 	for (i = 1; i < GRID_W; i++){
 		if (ret[i] == 1)
 			return (i);
-		if (ret[i] < ret[best])
+		else if (ret[i] < ret[best])
 			best = i;
-		if (ret[i] == ret[best])
-		{
-			if (get_bestindice(best, i))			
+		else if (ret[i] == ret[best] && get_bestindice(best, i))			
 				best = i;
-		}
 	}
 	return (best);
+}
+
+int		main(){
+	int	test[7] = {-1, -1000, -10000, -1, -100001000, -101000, -11000};
+
+	printf("result : %d\n", drop_piece(test));
+	return (0);
 }
