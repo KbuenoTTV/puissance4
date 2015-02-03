11f2e99b32bebf6fc7993ad501ee8fb511b3c94a
sle-guil <sle-guil@student.42.fr>
2015-02-03 14:26:08 +0100

    algo

--- algo_rec.c
+++ algo_rec.c
@@ -4,25 +4,36 @@ Algorithme recusion
 
 //	On peut utiliser une structure de retour specifique
 
-t_ret		rec (tab, last_drop, deepth)
+t_ret{
+	
+}
+
+t_ret		rec (tab, drop, deepth, color)
 
 	// Variable
 	int		i = 0;
 	t_ret	retour;
 
+	play(tab, drop, color)
 	// Condition de victoire
-	si (tab + last_drop == victoire)
-		return Victoire(deepth);
+	si (tab + last_drop*color == victoire)
+		return 1;
 
 	// Test des autre coup
 	sinon
+	{
 		tant que (i < largeur du tableau)
 		{
 			retour = rec (tab, i, deepth + 1)
-			sinon
-			{
-				// Calcul de ponderation
-			}
 			i++;
 		}
+		// Aanlyse des cas suivant
+		Si (victoire 100%)
+			return 1;
+		sinon si (defait > 0%)
+			return -1;
+		sinon
+			return 0;
+	}
+	unplay(tab, drop, color);
 }
