b31b24521f3e4155dfb82d36a999ae7972b1c286
sle-guil <sle-guil@student.42.fr>
2015-02-03 11:57:19 +0100

    first fix

--- suliac.c
+++ suliac.c
@@ -6,16 +6,16 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/03 11:35:38 by sle-guil          #+#    #+#             */
-/*   Updated: 2015/02/03 11:51:58 by sle-guil         ###   ########.fr       */
+/*   Updated: 2015/02/03 11:57:00 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <math.h>
 #include <time.h>
 
-int		drop_piece(t_grid *grid, int col, int ligne)
+int		drop_piece(t_grid *grid, int col)
 {
-	if ((*grid)[col][ligne] == VIDE)
+	if ((*grid)[col][0] == VIDE)
 		return (1);
 	return (0);
 }
