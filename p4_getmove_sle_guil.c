d79999086f8b0d019b99e004d3ee648d5928b0f0
Jonathan CHICHEPORTICHE <jchichep@e3r5p14.42.fr>
2015-02-03 11:45:14 +0100

    include

--- p4_getmove_sle_guil.c
+++ p4_getmove_sle_guil.c
@@ -6,9 +6,11 @@
 /*   By: jchichep <jchichep@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/03 11:39:25 by jchichep          #+#    #+#             */
-/*   Updated: 2015/02/03 11:39:54 by jchichep         ###   ########.fr       */
+/*   Updated: 2015/02/03 11:44:42 by jchichep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "puiss4.h"
 
 int     p4_getmove_sle_guil(t_grid *grid, t_case color, int turn_count)
 {
